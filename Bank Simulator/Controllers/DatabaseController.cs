9ac05034809ef02234f9bcbcc67985938f637a03
Musa <everythinginfo04@gmil.com>
2023-07-07 23:21:08 +0200

    Add sqlite database and two users

---
+++ Bank Simulator/Controllers/DatabaseController.cs
@@ -0,0 +1,45 @@
+﻿using Bank_Simulator.Database;
+using Bank_Simulator.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Scaffolding.Metadata;
+
+namespace Bank_Simulator.Controllers
+{
+    [ApiController]
+    public class DatabaseController : Controller
+    {
+        private readonly DataContext _context;
+
+        public DatabaseController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("api/AddUser")]
+        public async Task<ActionResult<List<DatabaseModels>>> AddUser(DatabaseModels user)
+        {
+            _context.DatabaseModels.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.DatabaseModels.ToListAsync());
+        }
+        [HttpGet("api/GetAllUsers")]
+        public async Task<ActionResult<List<DatabaseModels>>> GetAllUsers()
+        {
+            // var users = await _context.Users.ToListAsync();
+            return Ok(await _context.DatabaseModels.ToListAsync());
+        }
+
+        [HttpGet("api/GetUser/{IDNumber}")]
+        public async Task<ActionResult<List<DatabaseModels>>> GetUser(string IDNumber)
+        {
+            var user = await _context.DatabaseModels.FindAsync(IDNumber);
+            if (user != null)
+            {
+                return Ok(user);
+            }
+            return BadRequest("User not found");
+        }
+    }
+}
