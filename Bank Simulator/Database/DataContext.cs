80f8c316523e601d852cb76763f734fa58a4593b
Musa <everythinginfo04@gmil.com>
2023-08-17 22:31:53 +0200

    Fix Db error

--- Bank Simulator/Database/DataContext.cs
+++ Bank Simulator/Database/DataContext.cs
@@ -1,12 +1,12 @@
-﻿//using Bank_Simulator.Models;
-//using Microsoft.EntityFrameworkCore;
+﻿using Bank_Simulator.Models;
+using Microsoft.EntityFrameworkCore;
 
-//namespace Bank_Simulator.Database
-//{
-//    public class DataContext: DbContext
-//    {
-//        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
-//        public DbSet<DatabaseModels> DatabaseModels => Set<DatabaseModels>();
+namespace Bank_Simulator.Database
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<DatabaseModels> DatabaseModels => Set<DatabaseModels>();
 
-//    }
-//}
+    }
+}
